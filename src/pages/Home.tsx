f58bc947bcd74b513e3dc13718388c049568d44f
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2020-03-03 17:22:40 -0300

    Add remove todo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import { IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { IonModal, IonContent } from '@ionic/react';
+import { IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonModal, IonContent, IonListHeader, IonLabel } from '@ionic/react';
+import { addCircle, pencil, trash } from "ionicons/icons";
 import React, { useState} from 'react';
 import ModalTodo from '../components/ModalTodo';
 import './Home.css';
@@ -8,37 +8,41 @@ const Home: React.FC = () => {
     const [todoList, setTodoList] = useState(Array<{name: string}>());
     const [showModal, setShowModal] = useState(false);
 
+    function addTodo(newTodo:any) {
+        setTodoList([ ...todoList, newTodo ])
+    }
+
+    function removeTodo(todoToRemove:any) {
+        setTodoList(todoList.filter(todo => todo.name !== todoToRemove.name));
+    }
+
     function renderTodos() {
         return todoList.map(todo => (
             <li key={String(todo.name)}>
-                <span>{todo.name}</span>
+                <span className="todo-title">{todo.name}</span>
+                <IonIcon className="todo-actions" size="26" color="red" icon={pencil} onClick={() => setShowModal(true)}>Plus</IonIcon>
+                <IonIcon className="todo-actions" size="26" icon={trash} onClick={() => removeTodo(todo)}>Plus</IonIcon>
             </li>
         ));
-    }
-
-    function addTodo(newTodo:any) {
-        setTodoList([ ...todoList, newTodo ])
     }
 
     return (
     <IonPage>
         <IonHeader>
-            <div className="header-content">
-                <IonToolbar>
-                    <IonTitle size="large">Anotações</IonTitle>
-                </IonToolbar>
-
-                <button onClick={() => setShowModal(true)}>Adicionar</button>
-            </div>
+            <IonToolbar>
+                <IonTitle size="large">Anotações</IonTitle>
+            </IonToolbar>
         </IonHeader>
         <IonContent>
-          <div className="container">
-            <div className="content">
-                <ul>
-                    {renderTodos()}
-                </ul>
-            </div>
-          </div>
+            <IonListHeader>
+                <div className="container">
+                    <IonLabel>Lista de anotações</IonLabel>
+                    <IonIcon size="26" icon={addCircle} onClick={() => setShowModal(true)}>Plus</IonIcon>
+                </div>
+            </IonListHeader>
+            <ul>
+                {renderTodos()}
+            </ul>
         </IonContent>
         <IonContent>
             <IonModal isOpen={showModal}>
